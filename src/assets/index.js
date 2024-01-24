23332c3159a0f83d6977b994bceff30d9362aac4
rabbanidev <golamrabbani227417@gmail.com>
2024-01-24 20:10:14 +0600

    assets ready

---
+++ src/assets/index.js
@@ -0,0 +1,58 @@
+import logo from "./logo.svg";
+import cartItem from "./cart-item.png";
+import deleteImg from "./delete.svg";
+import gear from "./gear.svg";
+import movieImg from "./movie.png";
+import ring from "./ring.svg";
+import shoppingCart from "./shopping-cart.svg";
+import star from "./star.svg";
+import tag from "./tag.svg";
+
+//movie-covers
+import avater from "./movie-covers/avatar.png";
+import ironMan from "./movie-covers/iron-man.png";
+import marriageStrory from "./movie-covers/marriage-strory.jpg";
+import onceInHo from "./movie-covers/once-in-ho.jpg";
+import painAndGain from "./movie-covers/pain-and-gain.jpg";
+import parasite from "./movie-covers/parasite.jpg";
+
+// icons
+import checkout from "./icons/checkout.svg";
+import commingSoon from "./icons/commingSoon.svg";
+import favourite from "./icons/favourite.svg";
+import moon from "./icons/moon.svg";
+import newRelease from "./icons/newRelease.svg";
+import sun from "./icons/sun.svg";
+import trending from "./icons/trending.svg";
+import watchLater from "./icons/watchLater.svg";
+
+export const assests = {
+  logo,
+  cartItem,
+  deleteImg,
+  gear,
+  movieImg,
+  ring,
+  shoppingCart,
+  star,
+  tag,
+  movie: {
+    avater,
+    ironMan,
+    marriageStrory,
+    onceInHo,
+    painAndGain,
+    parasite,
+  },
+
+  icons: {
+    checkout,
+    commingSoon,
+    favourite,
+    moon,
+    newRelease,
+    sun,
+    trending,
+    watchLater,
+  },
+};
